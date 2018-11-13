2f2b14e3efa5cc53201e5c88b9e6e4acc17e5eba
Sergey Pimenov <sergey@pimenov.com.ua>
2018-11-13 11:23:39 +0200

    add Metro 4 to wordpress

--- wp-metro4.php
+++ wp-metro4.php
@@ -1,11 +1,28 @@
 <?php
 
 /*
-Plugin Name: Wp Metro4
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Plugin Name: Metro4 for Wordpress
+Plugin URI: https://https://github.com/olton/wp_metro4
+Description: Integrate Metro 4 into Wordpress.
 Version: 1.0
-Author: serge
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Author: Sergey Pimenov
+Author URI: https://pimenov.com.ua
+License: MIT
 */
+
+
+function metro4_scripts(){
+    $parent_style = basename(get_template_directory_uri()) . "-style";
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'metro4_css',
+		"//cdn.metroui.org.ua/v4/css/metro-all.min.css",
+		array( $parent_style ),
+		wp_get_theme()->get('1.0.0')
+	);
+
+    wp_deregister_script('jquery');
+
+    wp_enqueue_script('jquery', "//code.jquery.com/jquery-3.3.1.min.js", array(), "3", true);
+    wp_enqueue_script('metro4_js', "//cdn.metroui.org.ua/v4/js/metro.js", array(), "4", true);
+}
+add_action("wp_enqueue_scripts", "metro4_scripts");
