b19ff934b52dd7eca078dc50eba8ff0a92bc4ccb
Sergey Pimenov <sergey@pimenov.com.ua>
2018-11-13 11:45:48 +0200

    add Metro 4 to wordpress

--- wp-metro4.php
+++ wp-metro4.php
@@ -11,9 +11,9 @@ License: MIT
 */
 
 function metro4_scripts(){
-    $parent_style = basename(get_template_directory_uri()) . "-style";
+    $parent_style = get_stylesheet() . "-style";
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'metro4_css',
+	wp_enqueue_style( 'metro4-style',
 		"//cdn.metroui.org.ua/v4/css/metro-all.min.css",
 		array( $parent_style ),
 		wp_get_theme()->get('1.0.0')
@@ -22,6 +22,6 @@ function metro4_scripts(){
     wp_deregister_script('jquery');
 
     wp_enqueue_script('jquery', "//code.jquery.com/jquery-3.3.1.min.js", array(), "3", true);
-    wp_enqueue_script('metro4_js', "//cdn.metroui.org.ua/v4/js/metro.js", array(), "4", true);
+    wp_enqueue_script('metro4-script', "//cdn.metroui.org.ua/v4/js/metro.js", array(), "4", true);
 }
 add_action("wp_enqueue_scripts", "metro4_scripts");
